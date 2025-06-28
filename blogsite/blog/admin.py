55a115c433912d9ad57d17d0773658e8044f9ed1
1LAV1 <lav3831@gmail.com>
2025-06-28 20:38:02 +0530

    file structure changed

--- blogsite/blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import blogsite_post
-
-# Register your models here.
-admin.site.register(blogsite_post)  # Register your models here, e.g., admin.site.register(YourModel)
